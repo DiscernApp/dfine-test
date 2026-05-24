dda60c05b30f52bb5fc7b6ba88cbc58a2759ce49
Mark Johnson <mark.johnson@crackerjackltd.com>
2026-05-24 17:19:03 +1000

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DfineApp from './DfineApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DfineApp />
+  </React.StrictMode>
+)
