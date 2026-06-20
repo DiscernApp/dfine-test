994c24e790ebd13955f3059accbfe70f63f2fd12
Mark Johnson <mark.johnson@crackerjackltd.com>
2026-06-20 11:12:04 +1000

    fix: correct import case to dfine-app

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import DfineApp from './DfineApp'
+import DfineApp from './dfine-app'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <DfineApp />
   </React.StrictMode>
 )
+
